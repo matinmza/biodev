0244545d213f4871a1bbe5aa2f9ea19380a933cc
matin <kingman.mza@gmail.com>
2025-03-23 13:01:49 +0330

    save

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="font-iran-sans text-2xl font-bold">متین ۱۲</div>
+      <div className="font-sf-pro text-2xl font-bold">matin 12</div>
+    </div>
+  );
+}
